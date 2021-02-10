173d4829a8ebb150cd640a16320442f67351cad4
Janae Quinones <janaemassa@gmail.com>
2021-02-09 20:06:53 -0500

    associations

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -1,2 +1,4 @@
 class Walk < ApplicationRecord
+    belongs_to :walker_id
+    belongs_to :dog
 end
--- app/models/walker.rb
+++ app/models/walker.rb
@@ -1,2 +1,5 @@
 class Walker < ApplicationRecord
+    has_many :comments #made about them
+    has_many :walks
+    has_many :dogs, through: :walks
 end
