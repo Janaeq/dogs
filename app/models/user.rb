effad9ae85971815f5b55af49c9742b3fe761e83
Janae Quinones <janaemassa@gmail.com>
2021-02-10 22:38:08 -0500

    has_secure_password and validations" "

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :dogs
     has_many :comments
+    has_secure_password
+
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
 end
