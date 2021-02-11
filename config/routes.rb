f3e674fd96b2353a24855ad6cb959baca7e5a8b5
Janae Quinones <janaemassa@gmail.com>
2021-02-10 22:14:37 -0500

    created sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'sessions#welcome'
   
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
+
+  get "signup" => "users#new"
+  post "signup" => "users#create"
+
   resources :walks
   resources :comments
   resources :walkers
