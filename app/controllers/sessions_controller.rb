e7e82d61921c315ec4d131f8503a215772273bd9
Janae Quinones <janaemassa@gmail.com>
2021-02-11 01:48:14 -0500

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,15 @@ class SessionsController < ApplicationController
     end
 
     def create
-
+        @user = User.find_by(username: params[:user][:username])
+        # byebug
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:message] = "Invalid Credentials. Please try again."
+            redirect_to login_path
+        end
     end
 
     def destroy
