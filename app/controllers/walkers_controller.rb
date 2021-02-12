417af005a425006c2346a52b23011215b7a037f3
Janae Quinones <janaemassa@gmail.com>
2021-02-11 20:14:23 -0500

    add instance variables

--- app/controllers/walkers_controller.rb
+++ app/controllers/walkers_controller.rb
@@ -2,10 +2,12 @@ class WalkersController < ApplicationController
     
     def index
         # list of all walkers in the system
+        @walkers = Walker.all
     end
 
     def show
         # show's the specific walker's info 
+        @walker = Walker.find_by(id: params[:id])
     end
 
 end
