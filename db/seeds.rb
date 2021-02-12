7a91b1a003363ba92d18bac32941cdae0edfa9ad
Janae Quinones <janaemassa@gmail.com>
2021-02-11 22:41:23 -0500

    add walker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+walker1 = Walker.create(name: "Andy", bio: "Hi there! I'm a student at Furry University studying Animal Science. In my free time I love spending time with big and small dogs alike! I am certified in animal first aid, so I am always prepared if your little (or big!!) one ever needs special attention. Can't wait to meet your furry friend!")
+walker2 = Walker.create(name: "Currile", bio: "I was never a dog lover until I rescued my little baby Tiger! Ever since, I have grown a passion for hanging with dogs. In my free time I am studying for my Medical College Admission Test, so hanging with your pets give me much needed stress relief! Some hobbies I have are reading, journaling, and collecting candles :)")
+walker3 = Walker.create(name: "Amber", bio: "Puppies are my passion! I love playing with them, taking them for a walk, and learning new things about their individual personalities! I have a toy yorkie of my own, and have grown up with dogs my entire life! I hope to be able to meet your puppies soon!")
+walker4 = Walker.create(name: "Michael", bio: "I'm a gamer, but you'll never catch me on the console without my 3 dogs next to me. They bring me so much joy! Eventually when I do step away from the game, I love getting my exercise in with your pups. I'll never say no to a new furry friend!")
