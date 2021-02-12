fb2d24638872599ebe57cbf231df17eb7afdbb38
Janae Quinones <janaemassa@gmail.com>
2021-02-12 18:51:19 -0500

    remove current user and logged in methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
-
-  private
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
+  include ApplicationHelper
   
 end
