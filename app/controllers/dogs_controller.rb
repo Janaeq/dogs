251462587dafa15e1d6e0d6e4d7b288bdc4b8070
Janae Quinones <janaemassa@gmail.com>
2021-02-11 22:25:33 -0500

    refactor @dog for create route

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -9,11 +9,9 @@ class DogsController < ApplicationController
         @dog = Dog.new
     end
 
-    def create
-        # create a new dog for the logged in user
-        @dog = Dog.new(dog_params)
-        # set the user_id to the user_id in the session
-        @dog.user_id = session[:user_id]
+    def create # create a new dog for the logged in user
+        # .build method automatically puts the user_id of the new dog = to current_user id
+        @dog = current_user.dogs.build(dog_params)
         if @dog.save
             redirect_to dog_path(@dog)
         else
@@ -30,6 +28,8 @@ class DogsController < ApplicationController
         # deletes the instance of the dog. can only be done if dog belongs to logged in user
     end
 
+    private 
+    
     def dog_params
         params.require(:dog).permit(:name, :age, :breed, :gender)
     end
