f491adc311b0e7ee0496958b3ca705e597bf136b
Janae Quinones <janaemassa@gmail.com>
2021-02-11 20:14:07 -0500

    add instance variables

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -2,18 +2,24 @@ class WalksController < ApplicationController
 
     def new
         # schedules a new walk
+        @walk = Walk.new
     end
 
     def create
         # schedules a new walk
+        @walk = Walk.new(walk_params)
     end
 
     def edit
         # edits details of a scheduled walk
+        @walk = Walk.find_by(id: params[:id])
     end
 
     def destroy
         # cancels walk
     end
 
+    def walk_params
+        params.require(:walk).permit(:date_time)
+    end
 end
