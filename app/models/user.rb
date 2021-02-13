579a7c79c64a880b9564442e8b1af95aff22a3e5
Janae Quinones <janaemassa@gmail.com>
2021-02-12 22:01:21 -0500

    add association between user and walks through dogs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_many :dogs
     has_many :comments
     has_many :walkers, through: :dogs #to ensure that a user can only comment on walkers that are associated with their dogs
+    has_many :scheduled_walks, through: :dogs, source: :walks
     has_secure_password
 
     validates :username, :email, presence: true
