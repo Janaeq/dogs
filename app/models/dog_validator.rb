7048859ceef7d489981020d1501614def989904b
Janae Quinones <janaemassa@gmail.com>
2021-02-19 06:31:41 -0500

    remove dog validator

--- app/models/dog_validator.rb
+++ app/models/dog_validator.rb
@@ -1,6 +1,6 @@
 class DogValidator < ActiveModel::Validator
     def validate(dog)
-        user = User.find(dog.user_id)
+        byebug
         if user.dogs.include?(Dog.find_by(name: dog.name))
             dog.errors.add :name, "- This dog already exists for you!"
         end
