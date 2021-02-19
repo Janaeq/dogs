490f5a1518326780378d528ead8a780290cd9dbb
Janae Quinones <janaemassa@gmail.com>
2021-02-19 06:02:46 -0500

    delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,10 @@
 class CommentsController < ApplicationController
-    
+    before_action :set_walker, only: [:index, :new]
     def index 
-        @walker = Walker.find_by(id: params[:walker_id])
     end
 
     def new
         # creates a new comment about a specific walker
-        @walker = Walker.find_by(id: params[:walker_id])
         @comment = Comment.new
     end
 
@@ -14,23 +12,26 @@ class CommentsController < ApplicationController
         # creates a new comment about a specific walker
         @comment = Comment.new(comment_params)
         if @comment.save
-            redirect_to walker_comments_path(params[:comment][:walker_id])
+            redirect_to walker_comments_path(@comment.walker_id)
         else 
             render :new
         end
     end
 
-    def edit
-        # edits a comment that the logged in user has made 
-    end
-
     def destroy
         # deletes a comment that the logged in user has made
+        comment = Comment.find(params[:id])
+        comment.destroy
+        redirect_to walker_comments_path (comment.walker_id)
     end
 
     private
 
     def comment_params
         params.require(:comment).permit(:entry, :walker_id, :user_id)
+    end
+
+    def set_walker
+        @walker = Walker.find_by(id: params[:walker_id])
     end
 end
