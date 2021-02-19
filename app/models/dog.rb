ea82e0cedeba82b9708119b25c27ac3884fc3df8
Janae Quinones <janaemassa@gmail.com>
2021-02-19 07:01:44 -0500

    remove dog validator

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,5 +5,13 @@ class Dog < ApplicationRecord
     has_many :walkers, through: :walks
 
     validates :name, :age, :breed, :gender, presence: true
-    
+    validates :age, numericality: { greater_than_or_equal_to: 0 }
+    validate :does_not_exist
+
+    # scope? name can be the same as long as user_id is different
+    def does_not_exist
+        if Dog.find_by(name: self.name, age: self.age, breed: self.breed, gender: self.gender)
+
+        end
+    end
 end
--- app/models/dog_validator.rb
+++
@@ -1,8 +0,0 @@
-class DogValidator < ActiveModel::Validator
-    def validate(dog)
-        byebug
-        if user.dogs.include?(Dog.find_by(name: dog.name))
-            dog.errors.add :name, "- This dog already exists for you!"
-        end
-    end
-end
