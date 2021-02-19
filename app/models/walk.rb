d1089e1731d6612f2831aab9476a955bdc066218
Janae Quinones <janaemassa@gmail.com>
2021-02-18 21:54:59 -0500

    add custom setter

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -2,5 +2,14 @@ class Walk < ApplicationRecord
     belongs_to :walker
     belongs_to :dog
 
-    accepts_nested_attributes_for :dog
+    validates :date_time, presence: true
+    validates_associated :dog
+    # accepts_nested_attributes_for :dog
+
+    def dog_attributes=(dog_params)
+        dog = Dog.find_or_create_by(dog_params)
+        if dog.valid?
+            self.dog = dog
+        end
+    end
 end
