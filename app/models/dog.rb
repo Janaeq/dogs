ad56ee1c6b274727f0b2573af0be9fa79b3ccf02
Janae Quinones <janaemassa@gmail.com>
2021-02-20 13:02:49 -0500

    validates name uniqueness with same user_id

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,12 +6,7 @@ class Dog < ApplicationRecord
 
     validates :name, :age, :breed, :gender, presence: true
     validates :age, numericality: { greater_than_or_equal_to: 0 }
-    validate :does_not_exist
 
     # scope? name can be the same as long as user_id is different
-    def does_not_exist
-        if Dog.find_by(name: self.name, age: self.age, breed: self.breed, gender: self.gender)
-
-        end
-    end
+    validates_uniqueness_of :name, scope: :user_id
 end
