a0aee946939c88738f46c11843e436f9febd5ca6
Janae Quinones <janaemassa@gmail.com>
2021-02-20 16:36:32 -0500

    moved logged in and current user methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
-  include ApplicationHelper
-  
+  before_action :redirect_if_not_logged_in
+  # accepts these methods in the view
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def redirect_if_not_logged_in
+    redirect_to login_path if !logged_in?
+  end
 end
