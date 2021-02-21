4294eb3fcaefc71132f15d40dc5b637fba3fa485
Janae Quinones <janaemassa@gmail.com>
2021-02-20 20:14:10 -0500

    DRYing code

--- app/helpers/walks_helper.rb
+++ app/helpers/walks_helper.rb
@@ -1,2 +1,18 @@
 module WalksHelper
+    
+    def new_dog_option(f)
+        if @dog
+            tag.p("Dog: #{@dog.name}")
+        else
+            render partial: "dogform", locals:{f: f} 
+        end
+    end
+
+    def walks_heading
+        if @dog
+            tag.h1("#{@dog.name}'s Scheduled Walks ")
+        else 
+            tag.h1("Scheduled Walks")
+        end
+    end
 end
