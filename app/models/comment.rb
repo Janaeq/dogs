43509c2d62f29ac52b94cbbca64839825ca8fd4e
Janae Quinones <janaemassa@gmail.com>
2021-02-20 20:13:50 -0500

    custom validators for date

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
     belongs_to :walker
 
     validates :entry, length: { in: 1..240 }
+
+    def display_date
+        self.created_at.strftime("%B %e")
+    end
 end
--- app/models/walk.rb
+++ app/models/walk.rb
@@ -5,6 +5,7 @@ class Walk < ApplicationRecord
     validates :date_time, presence: true
     validates_associated :dog
     # validate that the date is in the future 
+    validate :date_cannot_be_in_the_past
 
     # scope method to find upcoming walk
     scope :next_walk, -> { order(date_time: :desc).limit(1) }
@@ -13,5 +14,17 @@ class Walk < ApplicationRecord
         if !dog_params[:name].blank?
             self.dog = Dog.find_or_create_by(dog_params)
         end
+    end
+
+    def display_date
+        self.date_time.strftime("%A, %b %d") if self.date_time
+    end
+
+    def display_time
+        self.date_time.strftime("%I:%M%P") if self.date_time
+    end
+
+    def date_cannot_be_in_the_past
+        errors.add(:date_time, "can't be in the past") if date_time < Date.today
     end
 end
