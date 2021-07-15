4d554e08ce7e3ed409e53bb9794b7ed5c604c0d7
Janae Quinones <janaemassa@gmail.com>
2021-07-15 14:01:22 -0400

    fixed google OAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+# MIDDLEWARE - ALLOWS APPLICATION TO VISIT /auth/google_oauth2 FOR AUTHENTICATION
+# NEED DOT ENV FILE TO PUT CLIENT ID AND CLIENT SECRET INTO
+
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
-  end
+end
